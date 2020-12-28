011ff65840ba90ef0923139ccc3d4350fe62e933
aaronzinhoo <aaronzinho@ucla.edu>
2020-12-28 03:17:45 -0800

    [FEATURE]: add auto setup for vcxsrv & windows termial

--- bootstrap_extensions/windows-bootstrap.sh
+++ bootstrap_extensions/windows-bootstrap.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 source "$( pwd )/utils.sh"
 
-PROMPT="[ WindowsExtensionLoader ]"
+PROMPT="[ WindowsExtensionLoader ]:"
+
+if [ -z "$WSL_DISTRO_NAME" ]; then
+	echo_with_prompt "Not using WSL, exiting..."
+	return 1
+fi
+
+WINDOWS_USERNAME=$(cmd.exe /c echo %username% | tr -d '\r')
+WINDOWS_HOME="/mnt/c/Users/${WINDOWS_USERNAME}"
 
 if [ "$1" = "--work" ]; then
 	echo_with_prompt "using work windows terminal settings"
-	cp $( pwd )/windows/settings_work.json /mnt/c/Users/Aaronzinho/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json
+	cp "$( pwd )/windows/settings_work.json" "${WINDOWS_HOME}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
 else
 	echo_with_prompt "using personal windows terminal settings"
-	cp $( pwd )/windows/settings.json /mnt/c/Users/Aaronzinho/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/
+	cp "$( pwd )/windows/settings.json" "${WINDOWS_HOME}/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/"
 fi
+
+echo_with_prompt "adding vcxsrv script to windows home"
+cp "$( pwd )/windows/MyXsrv.bat" "${WINDOWS_HOME}"
+echo_with_prompt "adding script to registry"
+reg.exe add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v RunXsrv /d $(wslpath -w $(echo "${WINDOWS_HOME}/MyXsrv.bat"))
