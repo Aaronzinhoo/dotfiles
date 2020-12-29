784cc19b3f2de82e0b626d3c9a6ea2dd541e9843
aaronzinhoo <aaronzinho@ucla.edu>
2020-12-29 01:59:46 -0800

    [UPDATE]: remove nvm and pyenv related install since zsh manages it
    This commit adds zsh-nvm and a package for improved npm
    autosuggestions. It also removes alot of code related to starting and
    managing nvm and pyenv

--- bootstrap_extensions/zsh-bootstrap.sh
+++ bootstrap_extensions/zsh-bootstrap.sh
@@ -1,4 +1,5 @@
 . "$( pwd )/utils.sh"
+
 PROMPT="[ ZSHExtensionLoader ]: "
 PACKAGE_TO_CHECK="git"
 
@@ -10,12 +11,6 @@ fi
 
 echo_with_prompt "installing OMZ..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
-
-CUSTOM_PLUGINS=$HOME/.oh-my-zsh/custom/plugins
-
-if [ ! -d "${CUSTOM_PLUGINS}" ]; then
-    mkdir -p "${CUSTOM_PLUGINS}"
-fi
 
 echo_with_prompt "cloning plugins and themes for OMZ and ZSH"
 
@@ -33,24 +28,18 @@ git clone https://github.com/agkozak/zsh-z "${CUSTOM_PLUGINS}"/zsh-z
 
 git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins "${CUSTOM_PLUGINS}"/autoupdate
 
+git clone https://github.com/lukechilds/zsh-nvm "${CUSTOM_PLUGINS}"/zsh-nvm
+
+git clone https://github.com/lukechilds/zsh-better-npm-completion "${CUSTOM_PLUGINS}"/zsh-better-npm-completion
+
 echo_with_prompt "adding poetry completion to zsh"
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
-if [ ! -d "${CUSTOM_PLUGINS}/poetry" ];then
-	mkdir -p "${CUSTOM_PLUGINS}/poetry"
-fi
 poetry completions zsh > "${CUSTOM_PLUGINS}/poetry/_poetry"
 
 echo_with_prompt "adding necessary symlinks"
-
-if [ ! -d "${HOME}/.local/bin" ]; then
-	echo_with_prompt "creating local bin directory"
-	mkdir -p $HOME/.local/bin
-fi
-
 # link batcat to bat on local
 ln -s /usr/bin/batcat $HOME/.local/bin/bat
 
 echo_with_prompt "changing shell to ZSH lovliness"
-
 chsh -s $(which zsh)
