6eae7fb0fa66782a67acde1bf1befd6989d752d2
aaronzinhoo <aaronzinho@ucla.edu>
2021-01-06 21:22:42 -0800

    [FIX]: fix installations for nvm poetry and golang
    [FIX]: try to fix pyenv & poetry path env for installation
    
    [FIX]: pyenv fix
    
    [REVERT]: revert commenting out zsh shell command

--- bootstrap_extensions/pyenv-bootstrap.sh
+++ bootstrap_extensions/pyenv-bootstrap.sh
@@ -4,5 +4,9 @@ PROMPT="[ PyenvExtensionLoader ]: "
 
 echo_with_prompt "installing python for use in pyenv"
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
 pyenv install 3.8.6
 pyenv global 3.8.6
