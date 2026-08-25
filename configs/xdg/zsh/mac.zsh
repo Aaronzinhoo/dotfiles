034e7076c61839e5e03a74be14b73c047f272899
Aaron Gonzales <aaronzinho@ucla.edu>
2026-08-25 01:43:45 -0700

    feat: brew only for certain exports

--- configs/xdg/zsh/mac.zsh
+++ configs/xdg/zsh/mac.zsh
@@ -6,10 +6,10 @@ if (( $+commands[brew] )); then
     export LIBRARY_PATH="${librdkafka_prefix}/lib${LIBRARY_PATH:+:${LIBRARY_PATH}}"
     export C_INCLUDE_PATH="${librdkafka_prefix}/include${C_INCLUDE_PATH:+:${C_INCLUDE_PATH}}"
   fi
+  export FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+  export PKG_CONFIG_PATH=$(brew --prefix)/opt/openssl@3/lib/pkgconfig:/usr/local/Cellar/zlib/1.2.8/lib/pkgconfig:/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig
 fi
 
-export FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-export PKG_CONFIG_PATH=$(brew --prefix)/opt/openssl@3/lib/pkgconfig:/usr/local/Cellar/zlib/1.2.8/lib/pkgconfig:/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig
 
 # chrome
 alias chrome="open -a Google\ Chrome"
